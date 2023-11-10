378a3c2c9ec72c2223ec1eb9f16744ecf5f5d38f
Sajeet07 <sajitgurung829@gmail.com>
2023-11-10 13:44:48 +0545

    Login and SignUp page frontend completed

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebasedemo/pages/LogIn.dart';
+import 'package:firebasedemo/pages/SignUpPage.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  firebase_auth.FirebaseAuth firebaseAuth = firebase_auth.FirebaseAuth.instance;
+  void signup() async {
+    try {
+      await firebaseAuth.createUserWithEmailAndPassword(
+          email: "sajit829@gmail.com", password: "123456");
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SignUpPage(),
+    );
+  }
+}
---
+++ lib/pages/LogIn.dart
@@ -0,0 +1,158 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class LogInPage extends StatefulWidget {
+  const LogInPage({super.key});
+
+  @override
+  State<LogInPage> createState() => _LogInPageState();
+}
+
+class _LogInPageState extends State<LogInPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          color: Color.fromARGB(255, 0, 66, 43),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Log In",
+                style: TextStyle(
+                    fontSize: 35,
+                    color: const Color.fromARGB(255, 50, 230, 65),
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              buttonItem("assets/google.svg", "Continue with Google", 25),
+              SizedBox(
+                height: 20,
+              ),
+              buttonItem("assets/phone.svg", "Continue with Mobile", 30),
+              SizedBox(
+                height: 15,
+              ),
+              Text(
+                "Or",
+                style: TextStyle(color: Colors.white, fontSize: 19),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              textItem("Email..."),
+              SizedBox(
+                height: 15,
+              ),
+              textItem("password..."),
+              SizedBox(
+                height: 30,
+              ),
+              colorButton(),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "If you don't have an account? ",
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                  Text(
+                    "SignUp",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Forgot Password",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 17,
+                    fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget colorButton() {
+    return Container(
+      width: MediaQuery.of(context).size.width - 90,
+      height: 60,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        gradient: LinearGradient(colors: [
+          Color.fromARGB(255, 50, 230, 65),
+          Color.fromARGB(255, 50, 230, 65),
+          Color.fromARGB(255, 50, 230, 65)
+        ]),
+      ),
+      child: Center(
+        child: Text(
+          "Sign In",
+          style: TextStyle(color: Colors.white, fontSize: 24),
+        ),
+      ),
+    );
+  }
+
+  Widget buttonItem(String imagepath, String buttonName, double size) {
+    return Container(
+      width: MediaQuery.of(context).size.width - 60,
+      height: 60,
+      child: Card(
+        color: Color.fromARGB(255, 0, 66, 43),
+        elevation: 8,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+          side: BorderSide(width: 1, color: Colors.grey),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset(imagepath, height: size, width: size),
+
+            SizedBox(
+              width: 15,
+            ),
+            Text(
+              buttonName,
+              style: TextStyle(color: Colors.white, fontSize: 17),
+            ),
+            //SvgPicture.asset("assets/phone.svg"),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget textItem(String labeltext) {
+    return Container(
+      width: MediaQuery.of(context).size.width - 70,
+      height: 55,
+      child: TextFormField(
+        decoration: InputDecoration(
+            labelText: labeltext,
+            labelStyle: TextStyle(fontSize: 17, color: Colors.white),
+            enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(15),
+                borderSide: BorderSide(width: 1, color: Colors.grey))),
+      ),
+    );
+  }
+}
